6c06c718984fb85b859b1b4ab2b0b5113b71e1cd
Kalina <kalina.vitaly2016@yandex.ru>
2021-02-18 17:32:57 +0300

    Added data processing

--- ParseData.cpp
+++ ParseData.cpp
@@ -1,39 +1,49 @@
 #include "ParseData.h"
 
 void ParseData::parseString(const QString & str) {
+    QStringList dataList;
     QString updateString = str;
-    QMap<QPair<QString, qreal>, QPair<QString, qreal>> data;
-    qint32 firstIndex = updateString.indexOf("<td>036</td>\r\n");
+    QMap<QPair<QString, double>, QPair<QString, double>> dataMap;
     qint32 lastIndex = 0;
-    updateString.remove(0, firstIndex);
-    lastIndex = updateString.indexOf("</tbody>");
-    updateString.remove(lastIndex, updateString.length() - lastIndex);
+    QPair<QString, double> key;
+    QPair<QString, double> value;
 
     //
     // Удаляем лишнии символы
     //
+    updateString.remove(0, updateString.indexOf("<td>036</td>\r\n"));
+    lastIndex = updateString.indexOf("</tbody>");
+    updateString.remove(lastIndex, updateString.length() - lastIndex);
+
     while (updateString.contains(QRegExp("\\r\\n\\s*"))) { updateString.remove(QRegExp("\\r\\n\\s*")); }
     while (updateString.contains(QRegExp("<tr>"))) { updateString.remove(QRegExp("<tr>")); }
     while (updateString.contains(QRegExp("</tr>"))) { updateString.remove(QRegExp("</tr>")); }
 
-//    <td>826</td>
-//    <td>GBP</td>
-//    <td>1</td>
-//    <td>Фунт стерлингов Соединенного королевства</td>
-//    <td>102,4401</td>
-    QPair<QString, qreal> key;
-    QPair<QString, qreal> value;
-//    while (!updateString.isEmpty()) {
-//        updateString.remove("<td>\\d+</td>");
-//        updateString.remove("<td>");
-//        updateString.remove("</td>");
-//        key.first = updateString.leftJustified(updateString.lastIndexOf("<td>"));
-//    }
+    updateString.remove(QRegExp("<td>"));
+    updateString.replace(QRegExp(","), ".");
+    dataList = updateString.split("</td>", Qt::SkipEmptyParts);
+    //
+    //  Создаем пары ключ-значение
+    //
+    qint32 elem = 0;
+    for (qint32 i = 1; i < dataList.length(); ++i) {
+        elem = i % 5;
+        if (elem >= 1 && elem <= 4) {
+            if (elem == 1) {
+                key.first = dataList[i];
+            } else if (elem == 2) {
+                key.second = dataList[i].toDouble();
+            } else if (elem == 3) {
+                value.first = dataList[i];
+            } else if (elem == 4) {
+                value.second = dataList[i].toDouble();
+            }
+        } else {
+            dataMap.insert(key, value);
+            continue;
+        }
+    }
 
-//    updateString.remove("<td>\\d+</td>");
-//    updateString.remove("<td>");
-//    updateString.remove("</td>");
-//    key.first = updateString.leftJustified(updateString.lastIndexOf("<td>"));
-//    qDebug() << key.first;
-    qDebug() << updateString;
+    qDebug() << dataMap;
+    //qDebug() << dataList;
 }
